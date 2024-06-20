ddc730b483c7cdb1f2b27f825c48ffae5b1f311d
Anikenator <Anikenator90@gmail.com>
2024-06-20 21:58:48 +0300

    initial commit

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,7 @@
 public class BmiService {
     public double calculate(double height, int weight) {
         double result = weight / (height * height);
-        return result;
+        int index = (int) result;
+        return index;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
         double result = service.calculate(1.87, 98);
-        int index = (int) result;
-        System.out.println(index);
+
+        System.out.println(result);
     }
 }
