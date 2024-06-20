269018eee788711ad048b262b0a637d47a6b4688
Anikenator <Anikenator90@gmail.com>
2024-06-20 20:59:10 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
         double result = service.calculate(1.87, 98);
-
-        System.out.println(result);
+        int index = (int) result;
+        System.out.println(index);
     }
 }
